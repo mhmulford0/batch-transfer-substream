8bbc831aa9b05968e003d905ab87eb87147b3084
mulf <mhmulford0@gmail.com>
2023-10-18 17:34:34 -0500

    cleanup

--- sink/src/main.ts
+++ sink/src/main.ts
@@ -23,45 +23,6 @@ if (!process.env.MODULE) {
 
 const { SPKG_URL, STREAMINGFAST_KEY, MODULE } = process.env;
 
-// const fetchPackage = async () => {
-//   return await fetchSubstream(SPKG_URL);
-// };
-
-// const main = async () => {
-//   const pkg = await fetchPackage();
-//   const registry = createRegistry(pkg);
-
-//   const transport = createConnectTransport({
-//     baseUrl: "https://api.streamingfast.io",
-//     // @ts-expect-error type error
-//     interceptors: [createAuthInterceptor(STREAMINGFAST_KEY)],
-//     useBinaryFormat: true,
-//     jsonOptions: {
-//       typeRegistry: registry,
-//     },
-//   });
-
-//   const request = createRequest({
-//     substreamPackage: pkg,
-//     outputModule: MODULE,
-//     productionMode: true,
-//     startBlockNum: 4333100,
-//     stopBlockNum: 4333139,
-//   });
-
-//   // Iterate over blocks
-//   for await (const response of streamBlocks(transport, request)) {
-//     const output = unpackMapOutput(response, registry);
-
-//     if (output !== undefined && !isEmptyMessage(output)) {
-//       const outputAsJson = output.toJson({ typeRegistry: registry });
-//       console.log(outputAsJson);
-//     }
-
-//     // console.log(response.toJson());
-//   }
-// };
-
 const substream = await fetchSubstream(SPKG_URL);
 const registry = createRegistry(substream);
 const transport = createGrpcTransport({
